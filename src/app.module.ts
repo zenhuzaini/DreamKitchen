55fc96aa965d635d6c8d07bf4e3baff74bc042e7
Zen Huzaini <huzainimhd@gmail.com>
2022-03-12 17:01:51 +0100

    fix graphql dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
-import { GraphQLModule} from '@nestjs/graphql'
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UserModule } from './modules/user/user.module';
 @Module({
-  imports: [GraphQLModule.forRoot({
-      autoSchemaFile: true
-  })],
+  imports: [GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+      autoSchemaFile: true,
+      debug: false,
+      playground: false,
+  }), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
